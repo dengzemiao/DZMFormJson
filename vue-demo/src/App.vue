208600f4a663556104cca9cec8c2a81dc8a71fd9
dengzemiao <dengzemiaovip@163.com>
2020-06-04 17:52:26 +0800

    添加vue-demo

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <input type="file" @change="read">
+  </div>
+</template>
+<script>
+import { formJson } from './form-json/form'
+export default {
+  methods: {
+    read (e) {
+      const file = e.target.files[0]
+      formJson(file, (code, data) => {
+        console.log(code, data)
+      })
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
